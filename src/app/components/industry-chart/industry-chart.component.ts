2465d63b374f65bf945c748ad8ca4666be6dc3a6
marek <marek@neugelb.com>
2019-12-14 11:50:02 +0100

    Chart

--- src/app/components/industry-chart/industry-chart.component.ts
+++ src/app/components/industry-chart/industry-chart.component.ts
@@ -3,10 +3,14 @@ import { CompanyStocks } from '../../reducers/basket.reducer';
 import { PriceService } from '../../services/price.service';
 import { IndustryService } from '../../services/industry.service';
 import { industries } from '../../data/industries';
+import { Label } from 'ng2-charts';
+import { ChartOptions, ChartType } from 'chart.js';
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 
 interface AmountByIndustry {
   amount: number;
   industryIds: number[];
+  industryNames: string[];
 }
 
 @Component({
@@ -19,6 +23,25 @@ export class IndustryChartComponent implements OnInit, OnChanges {
   @Input() companyStocks: CompanyStocks[];
   // produce an array where you can see a
   public amountByIndustry: AmountByIndustry[];
+  public pieChartLabels: Label[] = [];
+  public pieChartData: number[] = [];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = false;
+  public pieChartPlugins = [pluginDataLabels];
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartColors = [];
 
   constructor(private priceService: PriceService, private industryService: IndustryService) { }
 
@@ -29,6 +52,8 @@ export class IndustryChartComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     this.amountByIndustry = [];
+    this.pieChartData = [];
+    this.pieChartLabels = [];
     this.goThroughData();
   }
 
@@ -36,23 +61,35 @@ export class IndustryChartComponent implements OnInit, OnChanges {
     this.companyStocks.map((companyStock: CompanyStocks) => {
       this.manageData(companyStock);
     });
+    // now fill the chart data
+    this.amountByIndustry.map((foundItem: AmountByIndustry) => {
+      this.pieChartLabels.push(foundItem.industryNames);
+      this.pieChartData.push(foundItem.amount);
+    });
   }
 
   manageData(companyStock: CompanyStocks) {
     // go through array and check if industry already exists
     // if yes, add amount
     // if not, then add the object
-    const foundItems: AmountByIndustry[] = this.amountByIndustry.filter(x => x.industryIds === companyStock.company.industries);
-    console.log('foundItems : ', foundItems);
+    const industryIds = this.industryService.getTopLevelIndustry(companyStock.company.industries, 2);
+    const foundItems: AmountByIndustry[] = this.amountByIndustry.filter(x => {
+      return this.checkArrays(industryIds, x.industryIds);
+    });
     if (foundItems.length === 0) {
+      const industryNames = industryIds.map(industryId => this.industryService.iterateThroughChildren(industries, industryId).description);
       this.amountByIndustry.push({
         amount: this.priceService.getLatestPrice(companyStock),
-        industryIds: this.industryService.getTopLevelIndustry(companyStock.company.industries, 2)
+        industryIds,
+        industryNames
       });
     } else {
       foundItems[0].amount += this.priceService.getLatestPrice(companyStock);
     }
-    console.log('this.amountByIndustry : ', this.amountByIndustry);
+  }
+
+  checkArrays( arrA, arrB ) {
+    return arrA.sort().toString() === arrB.sort().toString();
   }
 
 }
