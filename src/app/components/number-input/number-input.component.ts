81225db701565f33dd53d2553a050295889ed659
marek1 <cyclingfellas@gmail.com>
2019-12-26 23:00:13 +0100

    Input of numbers in favs

--- src/app/components/number-input/number-input.component.ts
+++ src/app/components/number-input/number-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-number-input',
@@ -11,9 +12,21 @@ export class NumberInputComponent implements OnInit {
   @Input() smallestUnit: number;
   @Output() numberChanged = new EventEmitter<number>();
 
-  constructor() { }
+  // public AnzahlControl: FormControl;
+
+  constructor() {
+    // this.AnzahlControl = new FormControl();
+  }
 
   ngOnInit() {
+    // this.AnzahlControl.patchValue(this.anzahl);
+    // this.AnzahlControl.valueChanges.subscribe((val) => {
+    //   console.log('setting value : ', this.anzahl, ' ,,, val: ', val);
+    //   if (val !== this.anzahl) {
+    //     this.anzahl = val;
+    //     this.numberChanged.emit(val);
+    //   }
+    // });
   }
 
   decrease() {
@@ -24,5 +37,12 @@ export class NumberInputComponent implements OnInit {
   increase() {
     this.anzahl = this.anzahl + this.smallestUnit;
     this.numberChanged.emit(this.anzahl);
+  }
+
+  changeAnzahl(event: any) {
+    if (isNaN(parseInt(event.target.value, 10))) {
+      return;
+    }
+    this.numberChanged.emit(parseInt(event.target.value, 10));
   }
 }
